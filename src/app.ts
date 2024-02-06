661a206e704f7ebae766f90422c1fc348a381bb1
hugoniquen <hugo.niquen@bm-maritima.pe>
2024-02-06 12:16:06 -0500

    Hasta aca todo funciono y se instalo docker

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import config from './config';
 import cors from 'cors';
 import { MysqlConnection } from './database/MysqlConnection';
 import { MysqlStudentRepository } from './repositories/MysqlStudentRepository';
+import { StudentService } from './services/StudentService';
+import { StudentController } from './controllers/StudentController';
+import { scopePerRequest } from 'awilix-express';
 
 const app: Express = express();
 
@@ -15,6 +18,23 @@ container.register({
     dbConfig: asValue(config.dbConfig),
     dbConnection: asClass(MysqlConnection).singleton(),
     studentRepository: asClass(MysqlStudentRepository).scoped(),
+    studentService: asClass(StudentService).scoped(),
+    studentController: asClass(StudentController).scoped(),
 });
 
 app.use(cors());
+app.use(scopePerRequest(container));
+app.use(express.json());
+
+
+// cuando se usa esto (req, res, next) se le suele llamar Meddleware.
+app.use('/students',(req, res, next)=>{
+    container.resolve('studentController').router(req, res, next);
+});
+
+const PORT = process.env.PORT;
+const server = app.listen(PORT, ()=>{
+    console.log(`Server ejecutandose en http:localhost:${PORT}`);    
+});
+
+export = {app, server}
--- src/controllers/StudentController.ts
+++ src/controllers/StudentController.ts
@@ -1,6 +1,19 @@
-import { route } from "awilix-express";
+import { GET, route } from "awilix-express";
+import { Request, Response, Router } from 'express';
+import { StudentService } from "../services/StudentService";
 
 @route('/students')
 export class StudentController {
+    public router: Router;
+    
+    constructor(private readonly studentService : StudentService ) {        
+        this.router = Router();
+        this.router.get('/', this.all.bind(this));
+    }
 
-}
+    @GET()
+    public async all(req: Request, res: Response){
+        const students = await this.studentService.getAllStudents();
+        res.json(students);
+    }
+} 
